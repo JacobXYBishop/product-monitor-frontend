d2dde1e41b940b491c137c0a8234a437251b739b
Jacob <jacobxy@qq.com>
2017-10-26 10:54:16 +0800

    account.component update unit

--- src/app/account/account.service.ts
+++ src/app/account/account.service.ts
@@ -18,7 +18,20 @@ export class AccountService {
     return this.http.get(url).map(
       res => {
         const r = res.json();
-        return r.map(acct => new AccountModel(acct));
+        return r.map(acct => new AccountModel(this.modelTransform(acct)));
       });
+  }
+
+  modelTransform(obj) {
+    obj.stock_mv = (obj.stock_mv / 10000).toFixed(2);
+    obj.future_mv = (obj.future_mv / 10000).toFixed(2);
+    obj.future_mg = (obj.future_mg / 10000).toFixed(2);
+    obj.stock_total_asset = (obj.stock_total_asset / 10000).toFixed(2);
+    obj.future_total_asset = (obj.future_total_asset / 10000).toFixed(2);
+    obj.account_total_asset = (obj.account_total_asset / 10000).toFixed(2);
+    obj.exposure = (obj.exposure / 10000).toFixed(2);
+    obj.trade_exposure = (obj.trade_exposure / 10000).toFixed(2);
+    obj.trade_amount = (obj.trade_amount / 10000).toFixed(2);
+    return obj;
   }
 }
